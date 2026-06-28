225c849c4a90ba5a67127dd3b66391ffb6faeaa1
Joaquin Ambrosetti <joacoambrosetti@gmail.com>
2026-06-27 23:56:05 -0300

    feat: Propuesta de nueva navbar y formato de pagina aplicado a todos los html, cambios menores en main.js y estilos.css para que el modo oscuro se aplique a todas las sections y cambios en los tonos de los color

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
-/*boton modo oscuro/claro*/
+/*
+   MODO OSCURO / CLARO
+*/
 
-/*variable id deboton*/
-const boton = document.getElementById("btn-modo-oscuro"); 
-/*variable id de imagen del boton*/
-const imagen = document.getElementById("imgBoton");
+document.addEventListener("DOMContentLoaded", () => {
 
+    const boton = document.getElementById("btn-modo-oscuro");
+    const imagen = document.getElementById("imgBoton");
+    boton.addEventListener("click", () => {
 
-boton.addEventListener("click", () => { 
-/*verifica si la imagen del boton es la de modo oscuro o modo claro y cambia la imagen y el alt del boton a la que corresponde en cada caso*/
-    if (imagen.src.includes("dark_mode")) {
-        imagen.src = "imgs/light_mode_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png";
-        imagen.alt = "Modo Claro";
-    } else {
-        imagen.src = "imgs/dark_mode_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png";
-        imagen.alt = "Modo Oscuro";
-    }
-/*agrega o quita la clase modooscuro a los elementos del body, h1, section y cards para cambiar el estilo de la pagina*/
- document.body.classList.toggle("modooscuro");  
+        // Activa o desactiva el modo oscuro
+        document.body.classList.toggle("modooscuro");
 
- document.querySelector("h1").classList.toggle("modooscuro");
+        // Cambia el icono del botón
+        if (document.body.classList.contains("modooscuro")) {
 
- document.querySelector("section").classList.toggle("modooscuro");
+            imagen.src = "imgs/light_mode_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png";
+            imagen.alt = "Modo Claro";
 
- /*cambia el color de fondo de las cards de bootstrap*/
- document.body.querySelectorAll(".card").forEach((card) => {
-    card.classList.toggle("text-bg-dark");
-    card.classList.toggle("text-bg-success");
-  })
+        } else {
 
- });
+            imagen.src = "imgs/dark_mode_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png";
+            imagen.alt = "Modo Oscuro";
+
+        }
+
+        // Cambia el aspecto de las cards de bootstrap
+        document.querySelectorAll(".card").forEach(card => {
+            card.classList.toggle("text-bg-dark");
+        });
+
+    });
+
+});
